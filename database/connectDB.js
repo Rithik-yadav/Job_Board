1a3f5d94e8e94c617ffa00fdf5b91bb0079f3856
Chinmay Srivastava <chinmayy10@gmail.com>
2024-07-24 15:10:20 +0530

    logo change and changed made by rithik to jobzen and changed jobboard to jobzen

--- database/connectDB.js
+++ database/connectDB.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
     .connect(
-      "mongodb://127.0.0.1:27017",
-      // "mongodb+srv://RithikAdmin:1234@job-board.rz6nxuj.mongodb.net/?retryWrites=true&w=majority&appName=Job-Board",
+      "mongodb+srv://RithikAdmin:1234@job-board.rz6nxuj.mongodb.net/?retryWrites=true&w=majority&appName=Job-Board",
+      
       { dbName: "jobBoard" }
     )
     .then(() => {
