f2b761e39bccc6d7abe0cfa65faed094f4e40632
Rithik Yadav <cow17boy@gmail.com>
2024-07-28 11:52:49 +0530

    Reverted changes

--- app.js
+++ app.js
@@ -36,11 +36,5 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Internal Server Error");
 });
-const connectDB = require("./database/connectDB");
-
-connectDB();
-app.listen(5001, (req, res) => {
-  console.log("http://localhost:5001/");
-});
 
 module.exports = app;
